b41ca38dae64688cdb77f3c175379b1b991b005a
TNGDev <nqcuong2@gmail.com>
2020-07-04 10:44:53 -0500

    Project setup with basic TCP connection with a console server and Unity client

---
+++ GameServer/Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+
+        public int id;
+        public TCP tcp;
+
+        public Client(int clientID)
+        {
+            id = clientID;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int id)
+            {
+                this.id = id;
+            }
+
+            public void Connect(TcpClient socket)
+            {
+                this.socket = socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+                // TODO: send welcome packet
+            }
+
+            private void ReceiveCallback(IAsyncResult result)
+            {
+                try
+                {
+                    int byteLength = stream.EndRead(result);
+                    if (byteLength <= 0)
+                    {
+                        // TODO: disconnect
+                        return;
+                    }
+
+                    byte[] data = new byte[byteLength];
+                    Array.Copy(receiveBuffer, data, byteLength);
+
+                    // TODO: handle data
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {e}");
+                    // TODO: disconnect
+                }
+            }
+        }
+    }
+}
